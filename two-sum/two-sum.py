daf7ebbd3d6d8080e00b2f3415e7a6c14bb6f442
Jacob Villanueva <68416756+ja-pavi@users.noreply.github.com>
2021-11-29 12:19:55 -0600

    Time: 44 ms (99.89%), Space: 15.3 MB (54.78%) - LeetHub

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -1,6 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen = {}
+        
         for i, value in enumerate(nums):
             remaining = target - nums[i]
             
